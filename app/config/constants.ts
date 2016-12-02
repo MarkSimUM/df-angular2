96974147b1d859eef822b9a3bad5715c8cb39d0a
MarkBrightFutureApps <marksim@UMICH.EDU>
2016-12-02 10:53:05 -0500

    DF Settings

--- app/config/constants.ts
+++ app/config/constants.ts
@@ -1,2 +1,5 @@
-export const DREAMFACTORY_API_KEY: string = '819984e59b8d96961c827abc3fd05f1eba6b7913c286261396286225041d80c3';
-export const DREAMFACTORY_INSTANCE_URL: string = 'http://ft-djoker011.cl.dreamfactory.com';
+export const DREAMFACTORY_API_KEY: string = '05957f3603f770d0e326fae030f75374964b842b0c8cd3f1151b6a1e3ac9b73e';
+export const DREAMFACTORY_INSTANCE_URL: string = 'http://bitnami-dreamfactory-8a4f.cloudapp.net/';
+
+export const DREAMFACTORY_API_KEY_ORIG: string = '819984e59b8d96961c827abc3fd05f1eba6b7913c286261396286225041d80c3';
+export const DREAMFACTORY_INSTANCE_URL_ORIG: string = 'http://ft-djoker011.cl.dreamfactory.com';
